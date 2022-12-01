16fb16360f4ef3af0dd7ea9be2e3023ac6d5910a
DB55142 <44501551+DB55142@users.noreply.github.com>
2022-12-01 10:43:17 -0400

    Created player vessel wake
    created two float arrays to house preset values to set wake particle system with. Simulation space needs to be set to "world" in order for the particle system to work properly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,8 @@ public class PlayerController : MonoBehaviour
 
     private string[] presetSpeeds = new string[] { "-15kts", "-10kts", "-5kts", "0kts", "5kts", "10kts", "15kts", "20kts", "25kts", "30kts" };
     private float[] presetSpeedActual = new float[] { -5.95f, -5.94f, -5.9f, 0.0f, 5.9f, 5.94f, 5.95f, 6.0f, 6.2f, 6.5f };
+    private float[] presetSpeedWake = new float[] { -2.5f, -1.0f, -0.5f, 0.0f, 0.5f, 1.0f, 2.5f, 4.0f, 5.0f, 8.0f };
+    private float[] presetSpeedWakeLife = new float[] { 3, 2, 1, 0.01f, 1, 2, 3, 4, 5, 7 };
     private float[] rudderResistance = new float[] { -0.4f, -0.2f, -0.1f, 0.0f, 0.1f, 0.25f, 0.3f, 0.55f, 0.75f, 1.0f };
     private float[] rudderInput = new float[] { -40.0f, -20.0f, -10.0f, 0.0f, 10.0f, 20.0f, 40.0f };
     private float[] rudderAngleIndicatorValue = new float[] { 0.0f, 0.212f, 0.424f, 0.50f, 0.576f, 0.788f, 1.0f };
@@ -27,11 +29,14 @@ public class PlayerController : MonoBehaviour
     private int rudderAngleSelected = 3;
 
 
+    public ParticleSystem playerWake;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
@@ -79,9 +84,7 @@ public class PlayerController : MonoBehaviour
                 return;
             }
         }
-
-
-
+            
         speedTitle.text = "SPEED:";
 
         speedIndicator.text = presetSpeeds[presetSelected];
@@ -106,6 +109,15 @@ public class PlayerController : MonoBehaviour
         rigidBody.AddRelativeForce(Vector3.forward * Time.deltaTime * presetSpeedActual[presetSelected], ForceMode.Impulse);
 
         rigidBody.transform.Rotate(Vector3.up * Time.deltaTime * rudderInput[rudderAngleSelected] * rudderResistance[presetSelected]);
+
+        
+        var wake = playerWake.velocityOverLifetime;
+        wake.z = presetSpeedWake[presetSelected];
+        playerWake.startLifetime = presetSpeedWakeLife[presetSelected];
+
+
+
+
 
     }
 }
