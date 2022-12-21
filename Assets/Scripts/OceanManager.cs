70085ca1dc74c90a1cd088ae59a6409b97a20076
DB55142 <44501551+DB55142@users.noreply.github.com>
2022-12-21 10:12:49 -0400

    Water Splash Effect
    Added a particle system to spawn when a projectile hits the water

---
+++ Assets/Scripts/OceanManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanManager : MonoBehaviour
+{
+    //Class Variables
+    public ParticleSystem waterRipple;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Additional Functions
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainGunProjectile")
+        {
+            Vector3 position = other.transform.position;
+            Instantiate(waterRipple, position, waterRipple.transform.rotation);
+            Destroy(other);
+            
+        }
+
+        else if (other.gameObject.tag == "PlayerCIWS")
+        {
+            Vector3 position = other.transform.position;
+            Instantiate(waterRipple, position, waterRipple.transform.rotation);
+            Destroy(other);
+
+        }
+
+        else if (other.gameObject.tag == "PlayerMissile")
+        {
+            Vector3 position = other.transform.position;
+            Instantiate(waterRipple, position, waterRipple.transform.rotation);
+            Destroy(other);
+
+        }
+    }
+
+
+}
